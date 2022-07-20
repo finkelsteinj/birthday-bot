1d3b98a8151c3698218ebdf457dd99efdafa91ac
finkelsteinj <finkelsteinj11@gmail.com>
2022-07-19 20:00:06 -0400

    add coding outline

---
+++ birthday-bot.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+# GroupMe API Details
+access_token = '******'
+group_ID = '******'
+
+# Current Date
+datetimeObj = date.today()
+current_date = datetimeObj.strftime("%m/%d")  # ex: 07/11
+
+# Read 'birthday-list.csv' into Dictionary
+
+
+# Get Brothers' Last Names
+
+
+# Construct Message
+
+
+# Send Post
